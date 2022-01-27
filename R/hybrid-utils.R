b76e6fccf82f352bbfcd73e4e45ccecb0632a63c
ewarchul <ewarchul@gmail.com>
2022-01-27 13:46:44 +0100

    fix hybrid factory

--- R/hybrid-utils.R
+++ R/hybrid-utils.R
@@ -59,14 +59,15 @@ hybrid_factory <- function(des,
     }
     best_fitness <- min(des_result$best.fitness, cmaes_result$best.fitness)
 
-    best_val_log <- list(bestVal = c(
+    best_val_log <- (list(bestVal = matrix(c(
       des_result$diagnostic$bestVal,
       cmaes_result$diagnostic$bestVal
-    ))
+    ))))
 
     return(list(
       best.param = best_param,
       best.fitness = best_fitness,
+      value = best_fitness,
       n.evals = des_result$n.evals + cmaes_result$n.evals,
       label = "hybrid",
       diagnostic = best_val_log,
